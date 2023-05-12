4fa9bf232e1580dcdc0781ecbdc055e06680da9b
jankalucki <jan.kalucki@outlook.com>
2023-05-12 10:41:29 +0200

    bug fixes

--- bst.cpp
+++ bst.cpp
@@ -13,16 +13,28 @@ BST::~BST(){
 void BST::insert(double key){
     if (key > *this->value){
         if (this->right == NULL){
-            *this->right = BST(key);
+            this->right = new BST(key);
         }
         this->right->insert(key);
     }
     if (key < *this->value){
         if (this->left == NULL){
-            *this->left = BST(key);
+            this->left = new BST(key);
         }
         this->left->insert(key);
     }
+}
+bool BST::search(double key){
+    if (key == *this->value){
+        return true;
+    }
+    else if (key > *this->value){
+        this->right->search(key);
+    }
+    else{
+        this->left->search(key);
+    }
+    return false;
 }
 double BST::getvalue(){
     return *this->value;
@@ -31,12 +43,19 @@ void BST::show(){
     std::cout<<*this->value<<std::endl;
     if (this->left != NULL){
         this->left->show();
-    if (this->)
+    }
+    if (this->right != NULL){
+        this->right->show();
     }
 }
 
 int main(){
+    std::cout<<"aaaaaaaa";
     BST aaaaaaaa = BST(1.5);
     aaaaaaaa.insert(1.2);
-    std::cout<<aaaaaaaa.getvalue();
+    std::cout<<"aaaaaaaaaaaaaa\n";
+    aaaaaaaa.show();
+    std::cout<<aaaaaaaa.search(1.2)<<"\n";
+    std::cout<<aaaaaaaa.search(1.9);
+    return 0;
 }
