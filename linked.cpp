46be8bcf1c18dffb3d27445e3dc5ec6c2388d284
jankalucki <jan.kalucki@outlook.com>
2023-05-18 11:30:46 +0200

    Minimum and maximum determination added

--- linked.cpp
+++ linked.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "linked.h"
+#include <stdlib.h>
 template <class T> SinglyLinkedList<T>::SinglyLinkedList(T val){
     value = new T;
     *value = val;
@@ -21,7 +22,7 @@ template <class T> T SinglyLinkedList<T>::getvalue(){
     return *this->value;
 }
 template <class T> void SinglyLinkedList<T>::print(){
-    std::cout<<*this->value;
+    std::cout<<*this->value<<" ";
     if (this->next != NULL){
         this->next->print();
     }
@@ -42,9 +43,39 @@ template <class T> bool SinglyLinkedList<T>::search(T val){
     }
     
 }
+template <class T> T SinglyLinkedList<T>::maximum(){
+    T maxval=*this->value;
+    SinglyLinkedList<T>* head = this;
+    while (head->next != NULL){
+        if (*head->next->value > maxval){
+            maxval = *head->next->value;
+        }
+        head=head->next;
+    }
+    return maxval;
+}
+template <class T> T SinglyLinkedList<T>::minimum(){
+    T minval= *this->value;
+    SinglyLinkedList<T>* head = this;
+        while (head->next != NULL){
+            if (*head->next->value < minval){
+                minval = *head->next->value;
+            }
+            head = head->next;
+        }
+        return minval;
+}
 int main(){
-    SinglyLinkedList aaaaaaaaaa = SinglyLinkedList(2);
-    aaaaaaaaaa.insert(3,5);
-    aaaaaaaaaa.print();
-    std::cout<<aaaaaaaaaa.search(3)<<'\t'<<aaaaaaaaaa.search(69);
+    srand(time(0));
+    int tab[8];
+    for (int i = 0; i<8;i++){
+        tab[i] = rand() % 50;
+    }
+    SinglyLinkedList inst = SinglyLinkedList(2);
+    inst.insert(3,5);
+    for (int i =0; i<8;i++){
+        inst.insert(tab[i],i+1);
+    }
+    inst.print();
+    std::cout<< inst.search(3)<<'\t'<< inst.search(69)<<"\t"<< inst.maximum()<<"\t"<< inst.minimum();
 }
