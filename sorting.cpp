e4d366a0e2306c02db3f2418daade44e6df4aa18
Janek1804 <jan.kalucki@outlook.com>
2023-05-23 10:11:09 +0200

    mergesort added

--- sorting.cpp
+++ sorting.cpp
@@ -24,14 +24,57 @@ template <typename T> void quicksort(T* arr, int left, int right){
         quicksort(arr,partitionIndex+1,right);
     }
 }
-
+//TODO: review memory management on mergesort
+template<typename T> void merge(T* arr1, T* arr2, int length1, int length2){
+    int i =0;
+    int j =0;
+    int k =0;
+    T* result = new T[length1+length2];
+    while (i<length1 && j<length2) {
+        if (*(arr1+i)>*(arr2+j)){
+            *(result+k) = *(arr1+i);
+            i++;
+        }
+        else {
+            *(result+k) = *(arr2+j);
+            j++;
+        }
+        k++;
+    }
+    while (k<length1) {
+        *(result+k) = *(arr1+k);
+        k++;
+    }
+    while (k<length2) {
+        *(result+k) = *(arr2+k);
+        k++;
+    }
+}
+template <typename T> void mergesort(T* arr, int length){
+    int mid = (length-1) / 2;
+    int newlen = static_cast<int>(length/2);
+    T* arr1 = new T[newlen];
+    T* arr2 = new T[length - newlen];
+    int i = 0;
+    while (i<mid){
+        *(arr1+i) = *(arr+i);
+        i++;
+    }
+    while (i<length) {
+        *(arr2+i) = *(arr+i);
+        i++;
+    }
+    mergesort(arr1, newlen);
+    mergesort(arr2, length-newlen);
+    merge(arr1,arr2,newlen,length-newlen);
+}
 int main(){
     srand(time(NULL));
     double* arr = new double [10];
     for (int i=0; i<10; i++){
         *(arr+i) = (rand() % 100)/12.79;
     }
-    quicksort(arr,0,9);
+    mergesort(arr,10);
     for (int i=0;i<10;i++){
         std::cout<<*(arr+i)<<"\n";
     }
